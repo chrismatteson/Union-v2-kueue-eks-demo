bde8fcf02e53338cb68c2776568ecd8adb8887c0
Virgil Chan <virgil@union.ai>
2026-01-30 17:13:50 -0800

    FIX change `StringEquals` to `StringLike` in `iam.tf`

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -12,7 +12,7 @@ resource "aws_iam_role" "union_flyte" {
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
-          StringEquals = {
+          StringLike = {
             "${module.eks.oidc_provider}:sub" : "system:serviceaccount:*:*"
             "${module.eks.oidc_provider}:aud" : "sts.amazonaws.com"
           }
@@ -23,7 +23,7 @@ resource "aws_iam_role" "union_flyte" {
 
   tags = {
     Name        = "${var.cluster_name}-union-flyte-role"
-    Environment = "production"
+    Environment = "staging"
     Terraform   = "true"
   }
 }
